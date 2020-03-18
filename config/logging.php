595596f73f236f22e1cf2cac60dd4a165e43fb9e
康康 <uunmask2000@gmail.com>
2020-03-18 19:23:23 +0800

    trt

--- config/logging.php
+++ config/logging.php
@@ -70,9 +70,9 @@ return [
             'handler' => NullHandler::class,
         ],
 
-        'emergency'  => [
-            'path' => storage_path('logs/laravel.log'),
-        ],
+        // 'emergency'  => [
+        //     'path' => storage_path('logs/laravel.log'),
+        // ],
     ],
 
 ];
